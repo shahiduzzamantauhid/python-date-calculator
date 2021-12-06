94aa3380b86bda5755e1ecdfa1e9d6ca3274c9fa
tauhid143 <shahiduzzamantauhid@gmail.com>
2021-12-06 02:33:56 +0000

    second commit

--- date_calculator_by_python.py
+++ date_calculator_by_python.py
@@ -1,12 +1,17 @@
 # agr calculator
 #tale the age 
-age = int(input("What is your current age? \n"))
+c_age = int(input("What is your current age? \n"))
+age = 90 - c_age
 month = age*12
+week = age*52
 days = month*30
 hour = days*24
 
-# print(month, days, hour)
+# print("Your passing months are : " + str(month))
+# print("Your passing week are : " + str(week))
+# print("Your passing days are : " + str(days))
+# print("Your passing hours are : " + str(hour))
 
-print("Your passing months are : " + str(month))
-print("Your passing days are : " + str(days))
-print("Your passing hours are : " + str(hour))
+result = f"You have {month} months, {week} weeks, {days} days and {hour} hours"
+
+print(result)
